2fb7aee83301febc9f871c36cc046c69e74006da
fshangala <fshangala@gmail.com>
2022-05-13 08:48:20 +0200

    settings updated

--- accademica/accademica/settings.py
+++ accademica/accademica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&g$gi6uf@_)^d(&w3v$a%igbia+bhy^tm-9(5+1)8*0jam$y39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,6 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
