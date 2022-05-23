1917433248a76d08e1554be83ef35c96602ac568
fshangala <fshangala@gmail.com>
2022-05-23 23:56:23 +0200

    account changed to accounts

--- accademica/account/api/routes.py
+++ accademica/account/api/routes.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from account.api.viewsets import AccountViewset
 
 account_router = routers.SimpleRouter()
-account_router.register(r'account', AccountViewset)
+account_router.register(r'accounts', AccountViewset)
